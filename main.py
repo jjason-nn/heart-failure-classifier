0b401f690921be9bab17ab02bad143c884f8491f
jjason-nn <jason.nguyen2@ucalgary.ca>
2024-12-17 19:25:52 -0700

    Transformed data

--- main.py
+++ main.py
@@ -11,5 +11,25 @@ spark = SparkSession.builder.appName("Heart Failure Classifier").getOrCreate()
 data = spark.read.csv("heart.csv", inferSchema=True, header=True)
 
 #Data pipeline
-categorical_cols = ["Sex","ChestPainType","RestingECG","ST_Slope"]
+categorical_cols = ["Sex","ChestPainType","RestingECG", "ExerciseAngina", "ST_Slope"]
+
+# Transforming Categorical Columns to Numerical
 indexers = [StringIndexer(inputCol=col, outputCol=col + "_index") for col in categorical_cols]
+
+# Creating pipline for indexing categorical columns
+pipeline = Pipeline(stages=indexers)
+
+# Fit and Transform data
+data_transformed = pipeline.fit(data).transform(data)
+
+# data after transform
+print("transformed data\n")
+data_transformed.select(
+    "Sex", "Sex_index", 
+    "ChestPainType", "ChestPainType_index", 
+    "RestingECG", "RestingECG_index", 
+    "ST_Slope", "ST_Slope_index",
+    "ExerciseAngina", "ExerciseAngina_index"
+    ).show(5)
+
+spark.stop()
